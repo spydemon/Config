97247f238e034a2c3c156d3b49f7c3ac6b7d13a7
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2013-08-30 18:08:23 +0200

    Update of wmfs configuration for beeing usable with pulseaudio.

--- wm-scripts/batterie.sh
+++ wm-scripts/batterie.sh
@@ -5,5 +5,5 @@ if [ -z "$bat" ]; then
 	echo ""
 else
 #echo `echo $bat | cut -d " " -f4 | cut -b -3` " "
-	echo $bat | awk ' { print $4 } ' | tr "," " " 
+echo $bat | awk ' { print $4 } ' | tr "%," " " 
 fi
